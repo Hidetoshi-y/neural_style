db7a5a14b153d279831d0f5855b5f2a9493f9283
sky11fur <sky11fur@gmail.com>
2020-04-27 19:39:43 +0900

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 pip install keras || exit 1
 #pip install tensorflow || exit 1
-pip install tensorflow-gpu==1.14.0
+pip install tensorflow-gpu==1.13.1 || exit 1
 pip install pillow || exit 1
 pip install numpy || exit 1
 pip install scipy || exit 1
