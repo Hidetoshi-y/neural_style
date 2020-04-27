cee81cd034ab809bc412bf07acd601894fb57b35
sky11fur <sky11fur@gmail.com>
2020-04-27 18:49:34 +0900

    make requirement.txt

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pip install keras || exit 1
-pip install tensorflow || exit 1
-#pip install tensorflow-gpu==1.14.0
+#pip install tensorflow || exit 1
+pip install tensorflow-gpu==1.14.0
 pip install pillow || exit 1
 pip install numpy || exit 1
 pip install scipy || exit 1
